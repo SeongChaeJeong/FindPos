6b1f9b60330dab0ea7e8cb01f67f973614fd96b0
SeongChaeJeong <goopy684@gmail.com>
2017-03-05 23:09:36 +0900

    + 현재위치 만드는중

--- app/src/main/java/com/demco/goopy/findtoto/MapsActivity.java
+++ app/src/main/java/com/demco/goopy/findtoto/MapsActivity.java
@@ -1,20 +1,31 @@
 package com.demco.goopy.findtoto;
 
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+import java.security.Permission;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener {
 
     static final LatLng SEOUL = new LatLng(37.56, 126.97);
     private GoogleMap mMap;
+    private boolean mLocationPermissionGranted = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,5 +59,60 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         Marker seoul = googleMap.addMarker(new MarkerOptions().position(SEOUL).title("Seoul"));
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(SEOUL));
         googleMap.animateCamera(CameraUpdateFactory.zoomTo(10));
+        updateLocationUI();
+    }
+
+    private void updateLocationUI() {
+       if(mMap == null) {
+           return;
+       }
+
+       if(mLocationPermissionGranted) {
+           mMap.setMyLocationEnabled(true);
+           mMap.getUiSettings().setMyLocationButtonEnabled(true);
+       }
+       else {
+           mMap.setMyLocationEnabled(false);
+           mMap.getUiSettings().setMyLocationButtonEnabled(false);
+       }
+    }
+
+    private void drawMarker(Location location) {
+
+        //기존 마커 지우기
+        mMap.clear();
+        LatLng currentPosition = new LatLng(location.getLatitude(), location.getLongitude());
+
+        //currentPosition 위치로 카메라 중심을 옮기고 화면 줌을 조정한다. 줌범위는 2~21, 숫자클수록 확대
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom( currentPosition, 17));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(17), 2000, null);
+
+        //마커 추가
+        mMap.addMarker(new MarkerOptions()
+                .position(currentPosition)
+                .snippet("Lat:" + location.getLatitude() + "Lng:" + location.getLongitude())
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE))
+                .title("현재위치"));
+    }
+
+    //  http://developer88.tistory.com/31
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 }
