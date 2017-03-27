ab0abbf37cbd73a24fa7fa8aaf3b99ba4a1e4b5b
SeongChaeJeong <goopy684@gmail.com>
2017-03-27 16:22:42 +0900

    + 크래시레포트 기능 추가            + 리소스 파일 추가

---
+++ app/src/main/java/com/demco/goopy/findtoto/MyApplication.java
@@ -0,0 +1,25 @@
+package com.demco.goopy.findtoto;
+
+import android.app.Application;
+import android.support.multidex.MultiDexApplication;
+
+import org.acra.ACRA;
+import org.acra.ReportingInteractionMode;
+import org.acra.annotation.ReportsCrashes;
+
+/**
+ * Created by goopy on 2017-03-27.
+ */
+@ReportsCrashes(
+        mailTo = "goopy80@daum.net",
+        mode = ReportingInteractionMode.SILENT,
+        resToastText = R.string.error_crash
+)
+public class MyApplication extends MultiDexApplication {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        ACRA.init(this);
+    }
+}
